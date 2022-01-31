cf963f46312afa99b0f620803977a1afe06fcc94
TomaszMuchaDev <tomwasp5@gmail.com>
2022-01-31 18:46:03 +0000

    Docker containers for DB, App and docker-compose added.

--- src/main/java/com/napier/coursework/App.java
+++ src/main/java/com/napier/coursework/App.java
@@ -1,24 +1,115 @@
 package com.napier.coursework;
 
-public class App {
+import java.sql.*;
 
-    //global variables
-    private mySQLConnection SQL;
+public class App
+{
+    public static void main(String[] args) {
+        // Create new Application
+        App a = new App();
 
-    public void CreateConnection() {
+        // Connect to database
+        a.connect();
+        // Get Employee
 
-        SQL = new mySQLConnection("localhost:8888");
+        // Display results
+        a.getCity(10);
+
+        // Disconnect from database
+        a.disconnect();
+
     }
 
-    public static void main(String[] args)
+    // Connection to MySQL database.
+    private Connection con = null;
+    public void connect()
     {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
 
-        /*
-         * This the main method for the Group 6 application specified in the SET08803 Coursework
-         */
-
-
-        System.out.println("Boo yah!");
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
     }
+
+
+    // Disconnect from the MySQL database.
+
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+                System.out.println("Database connection closed.");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+
+    public String getCity(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =  "SELECT Name  FROM city WHERE ID = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Check one is returned
+            if (rset.next())
+            {
+                String name = rset.getString("Name");
+                System.out.println("City name is " + name);
+                return name;
+            }
+            else {
+                System.out.println("City  not found.");
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get City  details");
+            return null;
+        }
+    }
+
 
 }
