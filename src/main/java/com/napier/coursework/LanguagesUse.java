cf8d05d3166f8309db3b19613051ad653c81c492
avison9 <avisonlivingstone@gmail.com>
2022-02-19 19:27:03 +0100

    updating Uselanguage Java Class

---
+++ src/main/java/com/napier/coursework/LanguagesUse.java
@@ -0,0 +1,41 @@
+package com.napier.coursework;
+
+public class LanguagesUse {
+
+    private String name;
+    private String language;
+    private float percentage;
+    private int population;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
