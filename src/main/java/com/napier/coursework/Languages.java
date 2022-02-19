006dd76dded4f07c7e43bd9f2e0ab4a49945cc9f
avison9 <avisonlivingstone@gmail.com>
2022-02-19 20:36:35 +0100

    updating language java class

--- src/main/java/com/napier/coursework/Languages.java
+++ src/main/java/com/napier/coursework/Languages.java
@@ -1,41 +1,11 @@
 package com.napier.coursework;
 
-public class LanguagesUse {
+public class Languages {
 
-    private String name;
-    private String language;
-    private float percentage;
-    private int population;
+    public String name;
+    public String language;
+    public float percentage;
+    public int population;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public float getPercentage() {
-        return percentage;
-    }
-
-    public void setPercentage(float percentage) {
-        this.percentage = percentage;
-    }
-
-    public int getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(int population) {
-        this.population = population;
-    }
 }
