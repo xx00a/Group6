f33dd5d9f2ea9da1d3d623f9649bd9f0bc8f4714
avison9 <avisonlivingstone@gmail.com>
2022-02-19 19:32:24 +0100

    updating Uselanguage Java Class

--- src/main/java/com/napier/coursework/CapitalCity.java
+++ src/main/java/com/napier/coursework/CapitalCity.java
@@ -49,3 +49,43 @@ public class CapitalCity {
     }
 
 }
+
+public class LanguagesUse {
+
+    private String name;
+    private String language;
+    private float percentage;
+    private int population;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
