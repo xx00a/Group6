4cf7de5ea64ecb083029fbada9863dd5e8eb1a66
avison9 <avisonlivingstone@gmail.com>
2022-02-19 18:48:10 +0100

    update sql

--- sql/languages.sql
+++ sql/languages.sql
@@ -3,13 +3,13 @@ SELECT Sum(country.Population) AS "World Population" From country;
 
 #The population of a country.
 SELECT  country.Name AS Country, country.Continent AS Continent, country.Region AS Region, country.Population  FROM country WHERE country.Name = "Nigeria";
-
+SELECT  country.Name AS Country, country.Continent AS Continent, country.Region AS Region, country.Population  FROM country ORDER BY country.Name;
 #The population of a district.
 SELECT city.District, Sum(city.Population) AS Population FROM city  WHERE city.District = "New York";
-
+SELECT city.District, Sum(city.Population) AS Population FROM city  GROUP BY city.District;
 #The population of a city.
 SELECT city.Name AS City, country.Name AS Country, city.District, city.Population FROM city  INNER JOIN country  ON city.CountryCode = country.Code WHERE city.name = "Lagos";
-
+SELECT city.Name AS City, country.Name AS Country, city.District, city.Population FROM city  INNER JOIN country  ON city.CountryCode = country.Code ORDER BY city.Name;
 #the number of people who speak the following  languages from greatest number to smallest, including the percentage of the world population:
 #Speaking Chinese.
 SELECT countrylanguage.Language, Sum(country.Population) AS Speakers FROM countrylanguage  INNER JOIN country ON countrylanguage.CountryCode = country.Code WHERE countrylanguage.Language= "Chinese";
