08444c728864c5b100a67c0ede4f8abedffdafc8
xx00a <78671259+xx00a@users.noreply.github.com>
2022-02-19 23:21:59 -0500

    Closing out task #86 for review by team

--- src/main/java/com/napier/coursework/App.java
+++ src/main/java/com/napier/coursework/App.java
@@ -1,8 +1,14 @@
 package com.napier.coursework;
 
+/*
+ * SET08803 Coursework Application
+ *
+ */
+
 import java.sql.*;
 
 public class App {
+
     public static void main(String[] args) {
 
         // Connect to database
@@ -21,9 +27,18 @@ public class App {
         capitalCityReport.getWorldCapitalCitiesByPopulationDesc(connection);
 
 
+        // Start of Country Series of Reports -->
+        // Create new Country Report (from class)
         CountryReport countryReport = new CountryReport();
-        // Display results
-        countryReport.getWorldCountriesByPopulationDesc(connection);
+
+        //For now, we will use static references for N until interface is developed
+        //This will cycle through Report IDs 1 to 6
+
+        for (int x = 1; x < 7; x++) {
+            // Call on getReport method to create individual report
+            countryReport.getReport(x, connection);
+        }
+        // End of Country Series of Reports -->
 
 
         LanguagesReport languagesReport = new LanguagesReport();
---
+++ src/main/java/com/napier/coursework/Country.java
@@ -0,0 +1,15 @@
+package com.napier.coursework;
+
+/*
+ * The Country class is used to store results from the SQL database.
+ *
+ */
+
+public class Country {
+        public String Code;
+        public String Name;
+        public String Continent;
+        public String Region;
+        public long Population;
+        public String Capital;
+}
