7838ce86d4c14390fea402a30dd92d958533abaf
xx00a <78671259+xx00a@users.noreply.github.com>
2022-03-15 14:01:11 -0400

    Closing out task #110 for review by team to develop

--- src/main/java/com/napier/coursework/Country.java
+++ src/main/java/com/napier/coursework/Country.java
@@ -6,10 +6,59 @@ package com.napier.coursework;
  */
 
 public class Country {
-        public String Code;
-        public String Name;
-        public String Continent;
-        public String Region;
-        public long Population;
-        public String Capital;
+
+        private String code;
+        private String name;
+        private String continent;
+        private String region;
+        private long population;
+        private String capital;
+
+        public String getCode() {
+                return code;
+        }
+
+        public void setCode(String code) {
+                this.code = code;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getContinent() {
+                return continent;
+        }
+
+        public void setContinent(String continent) {
+                this.continent = continent;
+        }
+
+        public String getRegion() {
+                return region;
+        }
+
+        public void setRegion(String region) {
+                this.region = region;
+        }
+
+        public long getPopulation() {
+                return population;
+        }
+
+        public void setPopulation(long population) {
+                this.population = population;
+        }
+
+        public String getCapital() {
+                return capital;
+        }
+
+        public void setCapital(String capital) {
+                this.capital = capital;
+        }
 }
--- src/main/java/com/napier/coursework/Languages.java
+++ src/main/java/com/napier/coursework/Languages.java
@@ -1,11 +1,46 @@
 package com.napier.coursework;
 
+/*
+ * Comments are needed.
+ *
+ */
+
 public class Languages {
 
-    public String name;
-    public String language;
-    public float percentage;
-    public int population;
+    private String name;
+    private String language;
+    private float percentage;
+    private int population;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
